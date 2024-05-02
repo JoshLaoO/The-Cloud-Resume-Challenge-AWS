232496408a7cb6d8eec082d3ec80f03ae0741fae
Josh <jgarlao@gmail.com>
2024-05-02 16:55:40 -0500

    Header of Resume complete

---
+++ resume/src/Header.js
@@ -0,0 +1,32 @@
+function Header() {
+    const info = {
+        "fa-solid fa-phone": "469-463-4946",
+        "fa-solid fa-envelope": "jgarlao@gmail.com",
+        "fa-solid fa-location-dot": "Carrollton, TX - Open to remote",
+        "fa-brands fa-linkedin": "https://www.linkedin.com/in/josh-lao/",
+        "fa-brands fa-github": "https://gitlab.com/JoshLao",
+        "fa-brands fa-gitlab": "https://github.com/JoshLaoO"
+    }
+    return (
+    <div className="container">
+      <div className="row d-flex flex-row text-center">
+        <h3>Joshua Gar Lao</h3>
+        <h6>Full Stack Software Engineer</h6>
+      </div>
+      <div id="links" className='row d-flex w-50 text-center justify-content-center'>
+        { Object.keys(info).map((icon) => {
+            return(
+                <div className='col'><a className="text-decoration-none" href={info[icon]}><i className={icon}/></a></div>
+            )
+        })}
+        {/* <div>Phone - {info.phone}</div>
+        <div>Email - {info.email}</div>
+        <div>Location - {info.location}</div>
+        <div>Linkedin - <a href={info.linkedin}>Linkedin</a></div>
+        <div>Gitlab - {info.gitlab}</div>
+        <div>Github - {info.github}</div> */}
+      </div>
+    </div>
+  );
+}
+export default Header;
